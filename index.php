edeb22137b8960004fac6dbbe6085d3a78ef6007
kobroanggoro <kobroanggoro17@gmail.com>
2016-11-15 16:29:47 +0700

    tugas_praktik_tcc_praktik_9

--- index.php
+++ index.php
@@ -1,3 +1,26 @@
+<html>
+<body>
+<p>MENGHITUNG LUAS DAN KELILING LINGKARAN <br></p>
+<form name="form1" method="post" action="index.php">
+<pre>
+
+Masukkan Jari-Jari= <input type="text" name="jari"><br>
+<input type="submit" name="hitung" value="Hitung">
+
 <?php
-	echo "Hello World !!!";
+	$phi=22/7;
+	$r=$_REQUEST['jari'];
+	$luas=$phi*$r*$r;
+	$keliling= $phi*($r+$r);
+
+	extract($_POST);
+	if (isset ($hitung))
+	{
+		echo "LUAS LINGKARAN : $luas <br>
+KELILING LINGKARAN : $keliling";}
 ?>
+
+</pre>
+</form>
+</body>
+</html>
